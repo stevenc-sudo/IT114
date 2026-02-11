dc4c8df95e178fbc5e0a39a43a0d18ae5eee8534
Steven Cain <stevencain001@umb.edu>
2026-02-11 14:53:47 -0500

    Lab 1 pattern homework

---
+++ First_project.java
@@ -0,0 +1,10 @@
+public class First_project {
+    public static void main(String[] args) {
+        System.out.println(
+                "+''''''''''+\n" +
+                "[|  o  o  |]\n" +
+                " |    ^    |\n" +
+                " |   ' _ ' |\n" +
+                "+----------+\n");
+    }
+}
