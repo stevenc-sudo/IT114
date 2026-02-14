288f916545d88ac2fdf2ba567d9c86edb0c212d9
stevenc-sudo <steven.cain001@umb.edu>
2026-02-14 17:37:37 -0500

    Add Last exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, CS 114!");
+
+        System.out.println(
+                        "+------------+\n" +
+                        "[ |  o    o  | ]\n" +
+                        "  |     ^    |\n" +
+                        "  |   '__'   |\n" +
+                        "+------------+"
+        );
+    }
+
+}
